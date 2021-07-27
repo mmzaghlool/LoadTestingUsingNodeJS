766fdfaf4ae53b3a13e6050a9dc28d71baa6d406
Mostafa Mahmoud <mmzaghlool52@gmail.com>
2021-07-27 15:03:31 +0200

    add error logs and success rate

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
 "use strict";
 const childProc = require("child_process");
 const CHILD_PROCESSES = 20;
-const URL = 'https://www.google.com/';
+const URL = "https://google.com";
 
 (async () => {
   let times = [];
+  let errors = [];
   let children = [];
 
   for (let i = 0; i < CHILD_PROCESSES; i++) {
-    let childProcess = childProc.spawn("node", ["child.js", `--url=${URL}`])
+    let childProcess = childProc.spawn("node", ["child.js", `--url=${URL}`]);
     children.push(childProcess);
   }
 
   let responses = children.map(function wait(child) {
-    return new Promise(function c(res) {
-      child.stdout.on('data', (data) => {
-        console.log(`child stdout: ${data}`);
-        times.push(parseInt(data));
+    return new Promise(function c(resolve) {
+      child.stdout.on("data", (data) => {
+        if (typeof parseInt(data) === "number") {
+          console.log(`child stdout: ${data}`);
+
+          times.push(parseInt(data));
+        } else {
+          console.log(`child error: ${data}`);
+          errors.push(data + "");
+        }
       });
+
       child.on("exit", function (code) {
         if (code === 0) {
-          res(true);
+          resolve(true);
         } else {
-          res(false);
+          resolve(false);
         }
       });
     });
@@ -30,12 +38,16 @@ const URL = 'https://www.google.com/';
 
   responses = await Promise.all(responses);
 
-  if (responses.filter(Boolean).length == responses.length) {
-    const sum = times.reduce((a, b) => a + b, 0);
-    const avg = (sum / times.length) || 0;
-    console.log(`average: ${avg}`);
-    console.log("success!");
-  } else {
-    console.log("failures!");
-  }
+  console.log(`==========================================================`);
+
+  const sum = times.reduce((a, b) => a + b, 0);
+  const avg = sum / times.length || 0;
+  console.log(`average: ${avg}`);
+
+  const success = responses.filter((v) => v === true);
+  const successRate = success.length / responses.length;
+  console.log(`success rate: ${successRate}`);
+
+  // console.log(`==========================================================`);
+  // console.log("errors: ", errors.length, errors);
 })();
